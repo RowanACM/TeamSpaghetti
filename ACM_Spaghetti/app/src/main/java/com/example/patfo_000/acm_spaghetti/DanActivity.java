b0b87ab02d7800d650d744cbc9d5a53e6cd42fd1
sanchezer1757 <sanchezd6@students.rowan.edu>
2017-11-20 18:38:57 -0500

    Buttons and vibreations

--- ACM_Spaghetti/app/src/main/java/com/example/patfo_000/acm_spaghetti/DanActivity.java
+++ ACM_Spaghetti/app/src/main/java/com/example/patfo_000/acm_spaghetti/DanActivity.java
@@ -1,6 +1,11 @@
 package com.example.patfo_000.acm_spaghetti;
 
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +13,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class DanActivity extends AppCompatActivity {
+Vibrator vibrator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,15 +21,19 @@ public class DanActivity extends AppCompatActivity {
         setContentView(R.layout.activity_dan);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        final Vibrator vibe = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                assert vibe != null;
+                vibe.vibrate(VibrationEffect.createOneShot(40, VibrationEffect.DEFAULT_AMPLITUDE));
             }
         });
     }
+
 
 }
